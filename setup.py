8bd36ccaa51d6df49ac65dd379b6404406da096c
adka-fawaid <adkafawaid420@gmail.com>
2025-07-07 15:28:37 +0700

    Service Placement

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yafs-fog-computing",
+    version="0.2.0",
+    description="",
+    author="Hafid Nur",
+    author_email="hafidnurazis@gmail.com",
+    url="https://github.com/hafidnrzs/yafs-fog-computing",
+    package_dir={"": "."},
+    packages=find_packages(include=["yafs*"]),
+    python_requires=">=3.6",
+    install_requires=[
+        "simpy",
+        "pandas",
+        "networkx",
+        "numpy",
+        "tqdm",
+        "matplotlib",
+        "pulp",
+        "shapely",
+        "pyproj",
+        "scipy",
+        "smopy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
